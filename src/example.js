0e188e789fbdc5f78a545c9bd0e2e3bcf9af7d95
Sacha <sacha@grabyo.com>
2019-04-17 07:32:27 +0100

    change example gif

--- src/example.js
+++ src/example.js
@@ -4,7 +4,7 @@ import VideoContext from "videocontext";
 const vcCanvas = document.getElementById(`player`);
 const vc = new VideoContext(vcCanvas);
 
-const node = vc.customSourceNode(GIFNode, `https://media.giphy.com/media/KHWXGQRjbKrvUIjN2r/giphy.gif`);
+const node = vc.customSourceNode(GIFNode, `https://media1.giphy.com/media/4dDquQRZZOPyo/giphy.gif`);
 const node2 = vc.customSourceNode(GIFNode, `https://media.giphy.com/media/TrFTekH49d8yY/giphy.gif`);
 
 node.startAt(0);
